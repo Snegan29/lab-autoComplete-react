a7e8faa48c7f5e55f26314a883479a336a79f7e8
Snegan29 <pssj2914@gmail.com>
2023-12-19 13:41:48 +0530

    Files uploaded

---
+++ autoComplete/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import Countries from './resources/countryData.json'
+
+export default function App (){
+  const[query, setQuery] = useState("")
+
+  const handleKeyDown = (event) => {
+    if (event.keyCode === 27) {
+      setQuery("") 
+    }
+  }
+  return (
+    <div>
+      <input placeholder="Enter a letter" onChange={event => setQuery(event.target.value)} onKeyDown={handleKeyDown}/>
+      {
+        Countries.filter((country)=>{
+          if(query === ""){
+            return ""
+          } else if (country.name.toLowerCase().startsWith(query.toLowerCase())){
+            return country
+          }
+        }).map((country) => (
+          <div key={country.code}>
+            <p>{country.name}</p>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
